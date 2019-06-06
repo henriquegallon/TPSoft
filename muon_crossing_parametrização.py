4d63de90a66d9adcf3ea122669c32ada2b6cd43f
henriquegallon <50371025+henriquegallon@users.noreply.github.com>
2019-06-06 08:58:13 -0300

    1.1

--- muon_crossing_parametrização.py
+++ muon_crossing_parametrização.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 TPC = TPSoft.TPC(1000, 1000, 1000)
-Muon = TPSoft.Partícula(TPC.comprimento_x/2, 0, TPC.comprimento_z/2, np.pi/2, np.pi/2)
 fig = plt.figure('Parametrização - Múon')
 ax1 = fig.add_subplot(111, projection='3d')
 ax1.set_xlim(0, TPC.comprimento_x)
@@ -13,5 +12,6 @@ ax1.set_xlabel('x')
 ax1.set_ylabel('y')
 ax1.set_zlabel('z')
 TPC.plotar(ax1)
+Muon = TPSoft.Partícula(TPC.comprimento_x/2,  TPC.comprimento_y/2, TPC.comprimento_z/2, np.pi/2, np.pi/2)
 Muon.plotar(TPC, ax1)
 plt.show()
--- muon_crossing_z.py
+++ muon_crossing_z.py
@@ -2,7 +2,7 @@ from TPSoft import TPSoft
 import numpy as np
 import matplotlib.pyplot as plt
 
-TPC = TPSoft.TPC(1000, 1000, 1000)
+TPC = TPSoft.TPC(100, 1000, 1000)
 Muon = TPSoft.Partícula(TPC.comprimento_x/2, 0, TPC.comprimento_z/2, np.pi/2, np.pi/2)
 fig = plt.figure('Coleção - Histograma')
 ax1 = fig.add_subplot(121, projection='3d')
@@ -15,7 +15,7 @@ ax1.set_zlabel('z')
 TPC.plotar(ax1)
 Muon.plotar(TPC,ax1)
 ax2 = fig.add_subplot(122)
+TPC.coleção_de_luz(Muon, 0.01, ax2, x=0, tipo='Hist')
 ax2.set_xlim(0, TPC.comprimento_x)
 ax2.set_ylim(0, TPC.comprimento_y)
-TPC.coleção_de_luz(Muon, 0.01, ax2, y=1000, tipo='Hist')
 plt.show()
